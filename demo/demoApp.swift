2ccd8a18f02c87fc4f3ff42d9300c709795b96fc
Kapunahele Wong <kapunahelewong@gmail.com>
2025-11-05 12:28:51 -0800

    Initial Commit

---
+++ demo/demoApp.swift
@@ -0,0 +1,17 @@
+//
+//  demoApp.swift
+//  demo
+//
+//  Created by Kapunahele Wong on 11/5/25.
+//
+
+import SwiftUI
+
+@main
+struct demoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
